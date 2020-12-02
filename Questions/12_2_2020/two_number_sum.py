4e6471fb46a0e8cbb9346c4a851c5acb71f911dd
Khalil <khalilfoulks@gmail.com>
2020-12-02 15:40:34 -0500

    added a new problem

--- Questions/12_2_2020/two_number_sum.py
+++ Questions/12_2_2020/two_number_sum.py
@@ -1,3 +1,32 @@
+# input: 
+	# array of integers
+	# target sum
+
+# output
+	# array containing 2 numbers added together that equal target sum
+	# if no pair exists then return empty array
+
+# only one pair at MOST will equal target sum
+# pair of numbers can be in any order
+# input array will never be empty
+# intgers are all distinct
+# can't add a single integer by itself
+
+# ----------------PLAN------------------------------------------------
+# init n1 pointer
+	# n1 pointer starts at 0 index
+# init n2 pointer
+	# starts at 1 index
+# init empty ans array
+# loop through array checking if n1 + n2 = target
+# if so add both nums to ans array
+# otherwise increase n2 pointer by 1
+# repeat check
+# if n2 is at the end of array and n1 + n2 is not target increase n1 by 1
+# move n2 to 1 + n1
+# repeat until n1 is 1 less than # of elements in array
+
+
 def twoNumberSum(array, targetSum):
 	
 	ans_array = []
@@ -16,3 +45,10 @@ def twoNumberSum(array, targetSum):
 	return ans_array
 
 print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6],10))
+
+
+# n is the length of input array
+
+# time complexity: O(n^2) Nested for loop
+
+# space complexity: O(1) The only extra storage necessary is for the answer array which will always only hold a pair on nums or be empty. This means space is constant.
---
+++ Questions/12_2_2020/validate_subsequence.py
@@ -0,0 +1,34 @@
+# input
+# 2 seperate arrays
+
+# output
+# true 
+	# if 2nd array has numbers in the same sequential order as 1st array
+# false 
+	# if nums out of order
+	
+# single number in 1st array should output true
+# both arrays will never be empty
+
+#---------------------Plan------------------------------
+# init arr pointer
+# init sequence pointer
+# run as long as arr pointer and seq pointer are not larger than len of arr and len of seq
+# check if number in array at current index = number in sequence at current index
+	# if true increment seq array by 1
+# always increment arr index by 1
+# return true or false based on whether the entire sequence array has been traversed
+
+
+def isValidSubsequence(array, sequence):
+    # Write your code here.
+    
+    arrIdx = 0
+	seqIdx = 0
+	
+	while arrIdx < len(array) and seqIdx < len(sequence):
+		if array[arrIdx] == sequence[seqIdx]:
+			seqIdx += 1
+		arrIdx += 1
+	return seqIdx == len(sequence)
+			
