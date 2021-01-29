00adeab567b13d7485d75a13cde47b6cb6cff01a
Khalil <khalilfoulks@gmail.com>
2021-01-29 13:18:13 -0500

    added coments to optimal three_number_sum

--- Questions/arrays/three_number_sum_optimal.py
+++ Questions/arrays/three_number_sum_optimal.py
@@ -1,17 +1,30 @@
 def threeNumberSum(array, targetSum):
+    # sort the array
     array.sort()
+    # init empty array for answer
     ans = []
+    # loop through input array
     for i in range(len(array) - 2):
+        # init left pointer
         left = i + 1
+        # init right pointer
         right = len(array) - 1
+        # run until both pointers swap
         while left < right:
+            # add all numbers
             currentSum = array[i] + array[left] + array[right]
+            # check if cur sum is the target
             if currentSum == targetSum:
+                # if so add the triplet array to ans array
                 ans.append([array[i], array[left], array[right]])
+                # move left pointer 1 to the left
                 left += 1
+                # move right pointer one to the right
                 right -= 1
+            # if cur sum is lower than target, increase left pointer to get closer to target
             elif currentSum < targetSum:
                 left += 1
+            # if cur sum is greater than target, decrease right pointer to get closer to target
             elif currentSum > targetSum:
                 right -= 1
     return ans
