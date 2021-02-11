2ac0ac0f741901e0d1c331a0c3e33ba17a71ccfe
Khalil <khalilfoulks@gmail.com>
2021-02-11 15:54:10 -0500

    added smallest difference problem

---
+++ Questions/arrays/smallest_difference.py
@@ -0,0 +1,28 @@
+#-------------Plan----------------------
+
+# init min dif
+    # set to abs of max num from array1 - max from array 2
+
+# init ans array
+
+# loop through 1st array 
+    # loop through 2nd array
+        # if abs difference of num from 1st array - num from 2nd array is lower than min dif
+    # insert 2 values into ans array 
+    # min dif become abs(num1 - num2)
+
+
+def smallestDifference(arrayOne, arrayTwo):
+	minDif = abs(max(arrayOne) - max(arrayTwo))
+	ans = [0,0]
+	
+	for i in arrayOne:
+		for j in arrayTwo:
+			if abs(i - j) <=  minDif:
+				ans[0] = i
+				ans[1] = j
+				minDif = abs(i - j)
+	return ans
+
+# Time Complexity - O(n)
+# Space Complexity - O(1)
