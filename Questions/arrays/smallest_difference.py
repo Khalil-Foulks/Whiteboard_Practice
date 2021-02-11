6300381be2e9b4e8380ab6a80e914cc2ab3239ea
Khalil <khalilfoulks@gmail.com>
2021-02-11 16:22:37 -0500

    added optimal solution

--- Questions/arrays/smallest_difference.py
+++ Questions/arrays/smallest_difference.py
@@ -8,8 +8,8 @@
 # loop through 1st array 
     # loop through 2nd array
         # if abs difference of num from 1st array - num from 2nd array is lower than min dif
-    # insert 2 values into ans array 
-    # min dif become abs(num1 - num2)
+            # insert 2 values into ans array 
+            # min dif become abs(num1 - num2)
 
 
 def smallestDifference(arrayOne, arrayTwo):
@@ -25,3 +25,40 @@ def smallestDifference(arrayOne, arrayTwo):
 
 # Time Complexity - O(n^2)
 # Space Complexity - O(1)
+
+#---------------------------------------------------------------------------------
+#-------------------------------Optimal Solution----------------------------------
+
+#-------------Plan----------------------
+
+# init min dif
+    # set to abs of max num from array1 - max from array 2
+
+# init ans array
+# init idx counter
+
+# loop through 1st array 
+    # as long as idx != len(arrayTwo)
+        # store cur numTwo
+        # if abs difference of num from 1st array - num from 2nd array is lower than min dif
+            # insert 2 values into ans array 
+            # min dif become abs(num1 - num2)
+        # increment counter by 1
+    # reset idx counter to 0 and continue looping
+
+
+# def smallestDifference(arrayOne, arrayTwo):
+# 	min = abs(max(arrayOne) - max(arrayTwo))
+# 	ans = [0,0]
+# 	idx = 0 
+	
+# 	for num in arrayOne:
+# 		while idx != len(arrayTwo):
+# 			numTwo = arrayTwo[idx]
+# 			if abs(num - numTwo) <=  min:
+# 				ans = [num,numTwo]
+# 				min = abs(num - numTwo)
+# 			idx += 1
+# 		idx = 0
+	
+# 	return ans
