af6e94c58ab259f00e06420e32db7fecd1d997ca
Khalil <khalilfoulks@gmail.com>
2021-02-17 14:07:38 -0500

    added reverse words in string

---
+++ Questions/string_manipulation/reverse_words_in_string.py
@@ -0,0 +1,53 @@
+#-------------------------Plan-------------------------------
+
+# init words array = ''
+# init start of word idx = 0
+# loop through len of string
+    # store char
+
+    # if that char is an empty space
+        # slice the string to create a word
+        # append word to words arr
+        # update start of word to be idx of next word
+
+    # else if start of  a word is an empty space
+        # append a an empty space string
+        # update start of word to be idx of next word
+
+    # append final word to words arr  
+
+# init func reverse(list)
+    # init sart
+    # init end =0
+    # init len(list) - 1
+    # while start is < end
+        # swap first and last word
+        # move start one to the right
+        # move  end one the the left
+
+
+def reverseWordsInString(string):	
+	words = []
+	startOfWord = 0
+	
+	for idx in range(len(string)):
+		char = string[idx]
+		
+		if char == " ":
+			words.append(string[startOfWord:idx])
+			startOfWord = idx
+		elif string[startOfWord] == " ":
+			words.append(" ")
+			startOfWord = idx
+			
+	words.append(string[startOfWord:])
+	
+	reverseList(words)
+	return "".join(words)
+
+def reverse(text):
+	start, end = 0, len(list) - 1
+	while start < end:
+		list[start], list[end] = list[end], list[start]
+		start += 1
+		end -= 1
