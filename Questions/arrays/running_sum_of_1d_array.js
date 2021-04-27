328d235b8e0d2299ea9c801ba4f53ebb50d89f6f
Khalil <khalilfoulks@gmail.com>
2021-04-27 16:06:00 -0400

    added array problem

---
+++ Questions/arrays/running_sum_of_1d_array.js
@@ -0,0 +1,26 @@
+/**
+    create total = 0
+    create ans array
+    loop through array
+    add current num to total
+    insert total into ans array
+    return ans array
+ */
+
+
+var runningSum = function(nums) {
+    let ans = []
+    let total = 0
+    
+    for(let i = 0; i < nums.length; i++){
+        total += nums[i]
+        ans.push(total)
+    }
+    return ans
+};
+
+/**
+    time complexity - O(n) - where n is the # of ints in nums
+
+    space complexity - O(n) - where n is the # of ints in nums
+ */
