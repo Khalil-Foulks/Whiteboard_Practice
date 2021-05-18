35a462fb2c49d7983a5cea8889b4ad6274e1e52c
Khalil <khalilfoulks@gmail.com>
2021-05-18 13:06:08 -0400

    corrected answer

--- Questions/arrays/meeting_rooms_2.py
+++ Questions/arrays/meeting_rooms_2.py
@@ -7,9 +7,9 @@
 
 def minMeetingRooms(self, intervals):
     e = ret = 0
-    start = sorted(i.start for i in intervals)
-    end = sorted(i.end for i in intervals)
-    
+    start = sorted(i[0] for i in intervals)
+    end = sorted(i[1] for i in intervals)
+
     for s in range(len(start)):
         if start[s] < end[e]: 
             ret += 1
