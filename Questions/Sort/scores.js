b7cdbc31a9fb2fbf79b3da37d09bb850f3644b52
Khalil <khalilfoulks@gmail.com>
2021-07-21 18:14:55 -0400

    updated

--- Questions/Sort/scores.js
+++ Questions/Sort/scores.js
@@ -1,28 +1,28 @@
 function sortScores(unorderedScores, highestPossibleScore) {
 
-    // Array of 0s at indices 0..highestPossibleScore
-    const scoreCounts = new Array(highestPossibleScore + 1).fill(0);
-  
-    // Populate scoreCounts
-    unorderedScores.forEach(score => {
-      scoreCounts[score]++;
-    });
-  
-    // Populate the final sorted array
-    const sortedScores = [];
-  
-    // For each item in scoreCounts
-    for (let score = highestPossibleScore; score >= 0; score--) {
-      const count = scoreCounts[score];
-  
-      // For the number of times the item occurs
-      for (let time = 0; time < count; time++) {
-        sortedScores.push(score);
-      }
+  // Array of 0s at indices 0..highestPossibleScore
+  const scoreCounts = new Array(highestPossibleScore + 1).fill(0);
+
+  // Populate scoreCounts
+  unorderedScores.forEach(score => {
+    scoreCounts[score]++;
+  });
+
+  // Populate the final sorted array
+  const sortedScores = [];
+
+  // For each item in scoreCounts
+  for (let score = highestPossibleScore; score >= 0; score--) {
+    const count = scoreCounts[score];
+
+    // For the number of times the item occurs
+    for (let time = 0; time < count; time++) {
+      sortedScores.push(score);
     }
-  
-    return sortedScores;
+  }
+
+  return sortedScores;
 }
 
-// time complexity - O(n)
+// time complexity - O(n) 
 // space compexity - O(1)
