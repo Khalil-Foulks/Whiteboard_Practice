e16510476e9f7a8c3e6f0087a999045cb390e678
Khalil <khalilfoulks@gmail.com>
2021-08-16 15:13:38 -0400

    added min heap class example

--- Notes/heap.md
+++ Notes/heap.md
@@ -1,15 +1,72 @@
 # Heap
 
-### A Heap is a **special Tree**-based data structure in which the tree is a **complete binary tree**.
+### A Heap is a **special Tree** based data structure in which the tree is a **complete binary tree**.
 
 ## Types  
 -------
-![Min Heap](../images/tree/min_max_heap.bmp "Min Heap Tree")
+![Min & Max Heap](../images/tree/min_max_heap.bmp "Min & Max Heap Tree")
 
+```js
+// Min Heap
+let minHeap = [10,15,30,40,50,100,40]
+
+// Max Heap
+let maxHeap = [100,40,50,10,15,50,40]
+```
 ### Min Heap
 - The node value present at the root node must be **minimum** among the node values present at all of it’s children. 
 - The same property must be recursively true for all sub-trees in that Binary Tree. 
+- uses an array for storage
+
+```js
+// if starting at idx 0
+// Parent
+Math.floor((index - 2) / 2)
+
+// Left
+(index * 2) + 1
+
+// Right
+(index * 2) + 2
+```
+
+```js
+// if starting at idx 1 and idx 0 starting at null
+// Parent
+Math.floor(index / 2)
+
+// Left
+index * 2
+
+// Right
+index * 2 + 1
+```
 
 ### Max Heap
 - The node value present at the root node must be **highest** among the node values present at all of it’s children. 
 - The same property must be recursively true for all sub-trees in that Binary Tree.
+- uses an array for storage
+
+```js
+// if starting at idx 0
+// Parent
+(index - 2) / 2
+
+// Left
+(index * 2) + 1
+
+// Right
+(index * 2) + 2
+```
+
+```js
+// if starting at idx 1 and idx 0 starting at null
+// Parent
+(index - 2) / 2
+
+// Left
+(index * 2) + 1
+
+// Right
+(index * 2) + 2
+```
---
+++ Questions/heaps/min_heap_class.js
@@ -0,0 +1,110 @@
+// empty MinHeap that starts out empty
+// visualizer https://www.cs.usfca.edu/~galles/visualization/Heap.html
+
+class MinHeap{
+    constructor(){
+        this.storage = []
+        this.size = 0
+    }
+
+    // helper methods
+    getParentIdx(idx){
+        return Math.floor((index - 2) / 2);
+    }
+
+    getLeftChildIdx(idx){
+        return (index * 2) + 1;
+    }
+
+    getRightChildIdx(idx){
+        return (index * 2) + 2;
+    }
+
+    hasParent(idx){
+        return this.getParentIdx(idx) >= 0;
+    }
+
+    hasLeftChild(idx){
+        // returns boolean if left idx exists
+        return this.getLeftChildIdx(idx) < this.size;
+    }
+
+    hasRightChild(idx){
+        // returns boolean if right idx exists
+        return this.getRightChildIdx(idx) < this.size;
+    }
+
+    parent(idx){
+        return this.storage[this.getParentIdx]
+    }
+
+    leftChild(idx){
+        return this.storage[this.getLeftChildIdx]
+    }
+
+    rightChild(idx){
+        return this.storage[this.getRightChildIdx]
+    }
+
+    swap(idx1,idx2){
+        let temp = this.storage[idx1]
+        this.storage[idx1] = this.storage[idx2]
+        this.storage[idx2] = temp
+    }
+
+    insert(data){
+        this.storage[this.size] = data
+        this.size += 1
+        this.traverseUp();
+    }
+
+    // checks if last inserted node is in the correct position; if parent swap > last node and repeat check 
+    traverseUp(){
+        // grabs idx of last node
+        let idx = this.size - 1;
+        // if last node's parent exists and the parent value is > last node 
+        while(this.hasParent(idx) && this.parent(idx) > this.storage[idx]){
+            // swap the two node and repeat while moving up the tree 
+            this.swap(this.getParentIdx, idx)
+            idx = this.getParentIdx(idx)
+        }
+    }
+
+    // removes root node, makes last node the new root and runs traverseDown 
+    removeMin(){
+        // check if heap is empty
+        if(this.size === 0){
+            throw new Error("Cannot remove from empty heap.")
+        }
+
+        //root node
+        let data = this.storage[0]
+        // replace root with last node
+        this.storage[0] = this.storage[this.size - 1]
+        this.size -= 1
+        this.traverseDown()
+        return data
+    }
+
+    // checks if parent node (starting from root node) needs to swap with smallest child node;
+    traverseDown(){
+        let idx = 0;
+        while(this.hasLeftChild(idx)){
+            let smallestChildIdx = this.getLeftChildIdx(idx)
+            // if right child is smaller than left child; make right child, the smallest child
+            if(this.hasRightChild(idx) && this.rightChild(idx) < this.leftChild(idx)){
+                smallestChildIdx = this.getRightChildIdx(idx)
+            }
+            // if root is smaller than child; do nothing
+            if(this.storage[idx] < this.storage[smallestChildIdx]){
+                break
+            }
+            // else cur node and smallest node need to swap
+            else{
+                this.swap(idx,smallestChildIdx)
+            }
+            // repeat going down the tree
+            idx = smallestChildIdx
+        }
+    }
+}
