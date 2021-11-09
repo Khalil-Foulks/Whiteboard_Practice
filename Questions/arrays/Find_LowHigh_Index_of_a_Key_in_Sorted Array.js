73a713e59cc450d46bb3f6223a5ad19b50cc79e4
Khalil <khalilfoulks@gmail.com>
2021-11-09 17:56:47 -0500

    added new problem

---
+++ Questions/arrays/Find_LowHigh_Index_of_a_Key_in_Sorted Array.js
@@ -0,0 +1,9 @@
+let findLowIndex = function(arr, key) {
+    //TODO: Write - Your - Code
+    return -2;
+};
+  
+let findHighIndex = function(arr, key) {
+    //TODO: Write - Your - Code
+    return -2;
+};
