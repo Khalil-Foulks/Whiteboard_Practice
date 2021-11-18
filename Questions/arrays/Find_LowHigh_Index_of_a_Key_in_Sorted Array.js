c3c2cf102c9aa52265f488e60abb9206b142192f
Khalil <khalilfoulks@gmail.com>
2021-11-18 17:21:06 -0500

    added solution

--- Questions/arrays/Find_LowHigh_Index_of_a_Key_in_Sorted Array.js
+++ Questions/arrays/Find_LowHigh_Index_of_a_Key_in_Sorted Array.js
@@ -1,9 +1,53 @@
 let findLowIndex = function(arr, key) {
-    //TODO: Write - Your - Code
-    return -2;
-};
+    let low = 0;
+    let high = arr.length - 1;
+    let mid = Math.floor(high / 2);
+  
+    while (low <= high) {
+  
+      let midElem = arr[mid];
+  
+      if (midElem < key) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+  
+      mid = low + Math.floor((high - low) / 2);
+    }
+    
+    if (low < arr.length && arr[low] === key) {
+      return low;
+    }
+  
+    return -1;
+  };
   
 let findHighIndex = function(arr, key) {
-    //TODO: Write - Your - Code
-    return -2;
+    let low = 0;
+    let high = arr.length - 1;
+    let mid = Math.floor(high / 2);
+
+    while (low <= high) {
+        let midElem = arr[mid];
+
+        if (midElem <= key) {
+            low = mid + 1;
+        } 
+
+        else {
+            high = mid - 1;
+        }
+        mid = low + Math.floor((high - low) / 2);
+    }
+
+    if (high === -1){
+        return high
+    }
+
+    if (high < arr.length && arr[high] === key) {
+        return high;
+    }
+
+    return -1;
 };
