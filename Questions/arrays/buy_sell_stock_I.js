cb4d46c7c4ea368e55741f3107a0cf7b578dc056
Khalil <khalilfoulks@gmail.com>
2022-01-27 18:10:24 -0500

    added new problem

---
+++ Questions/arrays/buy_sell_stock_I.js
@@ -0,0 +1,17 @@
+var maxProfit = function(prices) {
+    let profit = 0;
+    let min = prices[0];
+    
+    for(let i = 1; i < prices.length; i++) { 
+        // if cur price is < min; min becomes cur price
+        if(min > prices[i]) {
+            min = prices[i];
+        } 
+        // update profit to be highest value
+        else if(prices[i] - min > profit) {
+            profit = prices[i] - min;
+        }
+    }
+    
+    return profit;
+};
