5b1ff37ba83bea37c82cff7d71110d0ee371f07f
Khalil <khalilfoulks@gmail.com>
2022-02-28 17:27:33 -0500

    added new linkedlist problem

---
+++ Questions/Linked_Lists/linked_list_cycle.js
@@ -0,0 +1,11 @@
+// class Node {
+//     constructor(value, next=null){
+//       this.value = value;
+//       this.next = next;
+//     }
+//   }
+  
+const has_cycle = function(head) {
+    // TODO: Write your code here
+    return false
+}
