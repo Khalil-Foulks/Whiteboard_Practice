b2213d47ff54fb830306b1b7707c780b488f66a6
Khalil <khalilfoulks@gmail.com>
2022-04-11 17:58:43 -0400

    added new problem

---
+++ Questions/BST/min_depth_of_binary_tree.js
@@ -0,0 +1,25 @@
+// class TreeNode {
+//     constructor(value) {
+//       this.value = value;
+//       this.left = null;
+//       this.right = null; 
+//     }
+// };
+  
+  
+const find_minimum_depth = function(root) {
+    // TODO: Write your code here
+    return -1;
+};
+
+
+
+var root = new TreeNode(12)
+root.left = new TreeNode(7)
+root.right = new TreeNode(1)
+root.right.left = new TreeNode(10)
+root.right.right = new TreeNode(5)
+console.log(`Tree Minimum Depth: ${find_minimum_depth(root)}`)
+root.left.left = new TreeNode(9)
+root.right.left.left = new TreeNode(11)
+console.log(`Tree Minimum Depth: ${find_minimum_depth(root)}`)
